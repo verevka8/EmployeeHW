9ec6841237c39cde6ee4cebdf5fc5cc152d3ba32
danii <daniillizunov6@gmail.com>
2025-02-28 11:15:43 +0300

    test

--- src/main/java/org/example/employee/EmployeeApplication.java
+++ src/main/java/org/example/employee/EmployeeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeeApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EmployeeApplication.class, args);
 	}
 }
